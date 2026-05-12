974a56c3a1382b90f257d9d5321ced7f7159604b
Ahmed-EsmaiL-web <ahmedmohamedismail2001@gmail.com>
2026-05-12 23:33:05 +0300

    refactor(app): integrate not-found and error middlewares in bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 import express from "express";
 import { config } from "./config/env.js";
 import connectDB from "./DB/connection.js";
+import { notFound } from "./middlewares/notFound.middleware.js";
+import { errorHandler } from "./middlewares/errorHandler.middleware.js";
 
 const bootstrap = async () => {
   // Initialize Express app
@@ -12,9 +14,17 @@ const bootstrap = async () => {
   // Middleware
   app.use(express.json());
   // Router handler
+  // Import and use routes
   app.get("/", (req, res) => {
     res.send("Welcome to the Saraha API!");
   });
+  // app.use("/api/v1/auth", authRoutes);
+  // app.use("/api/v1/users", userRoutes);
+  // app.use("/api/v1/messages", messageRoutes);
+  // Not Found Middleware (for unmatched routes)
+  app.use(notFound);
+  // Global Error Handler
+  app.use(errorHandler);
   // Start the server
   app.listen(config.PORT, () => {
     console.log(`Server is running on port ${config.PORT}`);
